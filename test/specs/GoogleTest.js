b646d90fc35a8c96a005ec7b8bb788b331ac1e9a
riesmarahman <riesmarahmann@gmail.com>
2025-03-25 12:06:23 +0700

    Add

---
+++ test/specs/GoogleTest.js
@@ -0,0 +1,18 @@
+const { expect } = require('@wdio/globals')
+const LoginPage = require('../pageobjects/login.page')
+const SecurePage = require('../pageobjects/secure.page')
+
+describe('Google Test', () => {
+    it('should open Google', async () => {
+        await browser.url('https://www.google.com');
+        await browser.pause(3000);
+
+        const title = await browser.getTitle();
+        console.log('Page title is: ' + title);
+        if (title === 'Google') {
+            console.log('Test Passed');
+        } else {
+            console.log('Test Failed');
+        }
+    });
+});
